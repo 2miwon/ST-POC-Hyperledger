d096c694e4faed22737accc5d5170b8d57ac0d70
2miwon <zhengsfsf@gmail.com>
2023-10-23 07:16:40 +0900

    add config bin builder

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo apt-get install git curl docker-compose -y
 sudo apt-get install golang -y
 sudo apt-get install jq -y
+sudo curl -sSL https://bit.ly/2ysbOFE | bash -s
+sudo systemctl start docker
