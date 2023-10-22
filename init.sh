9f9d326926b413dd03a13b9f12f7600ad395b3fa
2miwon <zhengsfsf@gmail.com>
2023-10-23 07:59:38 +0900

    .

--- init.sh
+++ init.sh
@@ -5,7 +5,8 @@ sudo curl -sSL https://bit.ly/2ysbOFE | bash -s
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
-cd ${PWD}/hyperledger/test-network
+cd ${PWD}/fabric-samples/test-network
+./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-go -ccl go
 export PATH=${PWD}/../bin:$PATH
 export FABRIC_CFG_PATH=$PWD/../config/
 export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
@@ -16,3 +17,4 @@ export CORE_PEER_LOCALMSPID="Org1MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 export CORE_PEER_ADDRESS=localhost:7051
+
