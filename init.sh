c7218f4b97139732fed18eb212a9126483f289bb
2miwon <zhengsfsf@gmail.com>
2023-10-23 07:30:55 +0900

    init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,15 @@ sudo apt-get install golang -y
 sudo apt-get install jq -y
 sudo curl -sSL https://bit.ly/2ysbOFE | bash -s
 sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+cd ${PWD}/hyperledger/test-network
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=$PWD/../config/
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+# Org1 설정
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
