fa4320bce93e359c71e35ed27bb61256e81e4a83
2miwon <zhengsfsf@gmail.com>
2023-11-02 07:40:33 +0900

    fix route

--- st-poc-client/src/app/api_test/page.tsx
+++ st-poc-client/src/app/api_test/page.tsx
@@ -1,4 +1,4 @@
-import { fetchChannelInfo } from '@/components/api/youtubeAPI';
+import { fetchChannelInfo } from '@/lib/youtubeAPI';
 
 export default async function Page() {
   const data = await fetchChannelInfo("UC_x5XG1OV2P6uZZ5FSM9Ttw")
